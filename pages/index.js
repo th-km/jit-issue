e59deaae30a4489e907ec6771dff45fb747497a0
Thomas Kim <35410212+th-km@users.noreply.github.com>
2021-05-12 10:44:20 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
+
+        {/* Leading class is ignored, while using @apply will generate an error */}
+        <h1 className="title leading-[1.125]">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
+      {/* Ignore */}
         <p className="mt-3 text-2xl">
           Get started by editing{' '}
           <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
